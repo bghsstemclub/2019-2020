f76a9969200132641917cbc5971f368fb32b8c42
VitaMinh D <minhduongstemclub@gmail.com>
2019-08-20 16:19:23 -0700

    fixing responsiveness and bug in the side menu

--- sideMenu.js
+++ sideMenu.js
@@ -7,12 +7,3 @@ function openButton() {
 function closeButton() {
   document.getElementById("side_menu_id").style.width = "0";
 }
-
-//show the extra links in the extra menu section
-function showingExtraMenu1() {
-  document.getElementById("extra_menu_id1").classList.toggle("show");
-}
-
-function showingExtraMenu2() {
-  document.getElementById("extra_menu_id2").classList.toggle("show");
-}
